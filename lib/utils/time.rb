c9d909afd410258f469152fe45876d8fa90a3240
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-12-28 15:40:06 -0800

    make methods in Prongs::Utils::Time visible

--- lib/utils/time.rb
+++ lib/utils/time.rb
@@ -5,7 +5,7 @@ module Prongs
 
       UTC = TZInfo::Timezone.get("UTC")
 
-      def duration_colon(d)
+      def self.duration_colon(d)
         total_seconds = (d / 1.second).to_f.round
         seconds = total_seconds % 60
         minutes = (total_seconds/60) % 60
@@ -13,19 +13,19 @@ module Prongs
         format("%02d:%02d:%02d", hours, minutes, seconds)
       end
 
-      def duration_seconds(d)
+      def self.duration_seconds(d)
         total_seconds = (d / 1.second).to_f.round
       end
 
-      def duration_hours(d)
+      def self.duration_hours(d)
         total_seconds = (d / 3600.second).to_f.round(2)
       end
 
-      def duration_days_floor(d)
+      def self.duration_days_floor(d)
         total_seconds = (d / (24*3600).second).to_f.floor
       end
 
-      def duration_string(d)
+      def self.duration_string(d)
         d = d/1.second
         dur_days = duration_days_floor(d)
         dur_hours = duration_hours(d - dur_days.days)
